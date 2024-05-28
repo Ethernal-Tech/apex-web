f196271d68f779877768839d7788dba80457558e
Zoltan Babinski <babinskizoltan@gmail.com>
2024-05-28 14:45:19 +0200

    remove console log

--- web-api/src/auth/auth.service.ts
+++ web-api/src/auth/auth.service.ts
@@ -65,7 +65,6 @@ export class AuthService {
 		if (!loginCode) {
 			throw new BadRequestException();
 		}
-		console.log('use pk: ' + process.env.USE_PRIVATE_KEY);
 
 		if (
 			process.env.USE_PRIVATE_KEY != 'true' &&
