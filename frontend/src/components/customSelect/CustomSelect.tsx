6a81c7e0032dc34b4795a6e42ee615b451158f44
Nikola <nlukic97@gmail.com>
2024-07-10 09:45:23 +0200

    network swithcer inputs added

---
+++ frontend/src/components/customSelect/CustomSelect.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Select, MenuItem, ListItemIcon, ListItemText, FormControl, InputLabel, SelectChangeEvent } from '@mui/material';
+// import { SvgIconComponent } from '@mui/icons-material';
+
+interface Option {
+  value: string;
+  label: string;
+  icon: React.FC;
+}
+
+interface CustomSelectProps {
+  label: string;
+  icon: React.FC;
+  value: string;
+  onChange: (event: SelectChangeEvent<string>) => void;
+  options: Option[];
+}
+
+const CustomSelect: React.FC<CustomSelectProps> = ({ label, icon: IconComponent, value, onChange, options }) => {
+  return (
+    <FormControl variant="outlined" fullWidth>
+      <InputLabel>{label}</InputLabel>
+      <Select
+        value={value}
+        onChange={onChange}
+        label={label}
+        renderValue={(selected) => (
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <ListItemIcon style={{ minWidth: 0, marginRight: 8 }}>
+              <IconComponent />
+            </ListItemIcon>
+            {options.find(option => option.value === selected)?.label}
+          </div>
+        )}
+      >
+        {options.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            <ListItemIcon style={{ minWidth: 0, marginRight: 8 }}>
+              <option.icon />
+            </ListItemIcon>
+            <ListItemText primary={option.label} />
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default CustomSelect;
--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -1,16 +1,66 @@
+import React, { useState } from "react";
+import { Typography, Box, Button } from '@mui/material';
+import CustomSelect from '../../components/customSelect/CustomSelect';
+import { ReactComponent as PrimeIcon } from '../../assets/chain-icons/prime.svg';
+import { ReactComponent as VectorIcon } from '../../assets/chain-icons/vector.svg';
+import { ReactComponent as NexusIcon } from '../../assets/chain-icons/nexus.svg';
+import { ReactComponent as SwitcherIcon } from '../../assets/switcher.svg';
 import BasePage from '../base/BasePage';
 import BridgeGraph from "../../assets/Bridge-Graph.svg";
-import { Typography, Box } from '@mui/material';
 
-// TODO AF - this is not ready yet
-// TODO AF - add is loading here for spinner
-const TransactionsTablePage = () => {
+const TransactionsTablePage: React.FC = () => {
+  const [source, setSource] = useState('prime');
+  const [destination, setDestination] = useState('vector');
+
+  const options = [
+    { value: 'prime', label: 'Prime', icon: PrimeIcon },
+    { value: 'vector', label: 'Vector', icon: VectorIcon },
+    { value: 'nexus', label: 'Nexus', icon: NexusIcon },
+  ];
+
+  const switchValues = () => {
+    const temp = source;
+    setSource(destination);
+    setDestination(temp);
+  };
+
+  const getIconComponent = (value: string): React.FC => {
+    const option = options.find(opt => opt.value === value);
+    return option ? option.icon : PrimeIcon; // Default to PrimeIcon if not found
+  };
+
   return (
     <BasePage>
-            <Typography variant="h1" sx={{ color: '#F25041', lineHeight:'128px', fontSize:'64px' }} fontFamily={'Major Mono Display, sans-serif'}>
-              Apex bridge
-            </Typography>
-            <img src={BridgeGraph} alt="apex bridge graph" />
+      <Typography variant="h1" sx={{ color: '#F25041', lineHeight: '128px', fontSize: '64px' }} fontFamily={'Major Mono Display, sans-serif'}>
+        Apex bridge
+      </Typography>
+      <img src={BridgeGraph} alt="apex bridge graph" />
+
+      <Box display="flex" alignItems="center" justifyContent="space-between" p={4}>
+        <Box>
+          <Box mb={1} fontWeight="bold">SOURCE</Box>
+          <CustomSelect
+            label="Source"
+            icon={getIconComponent(source)}
+            value={source}
+            onChange={(e) => setSource(e.target.value)}
+            options={options}
+          />
+        </Box>
+        <Button onClick={switchValues} sx={{mt:'20px', boxShadow:'none'}}>
+          <SwitcherIcon />
+        </Button>
+        <Box>
+          <Box mb={1} fontWeight="bold">DESTINATION</Box>
+          <CustomSelect
+            label="Destination"
+            icon={getIconComponent(destination)}
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            options={options}
+          />
+        </Box>
+      </Box>
     </BasePage>
   );
 };
