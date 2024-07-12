74c75cff6f362d7e6f430ac82707ede4b18f1689
Nikola <nlukic97@gmail.com>
2024-07-12 09:55:40 +0200

    paste text input made

--- frontend/src/pages/Transactions/TestNewTransactionPage.tsx
+++ frontend/src/pages/Transactions/TestNewTransactionPage.tsx
@@ -1,16 +1,46 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import BasePage from "../base/BasePage";
 import AddressBalance from "./components/AddressBalance";
+import TotalBalance from "./components/TotalBalance";
+import PasteTextInput from "./components/PasteTextInput";
 
 // TODO: add input validations
 function NewTransactionPage() {
 	return (
 		<BasePage>
-			<Box width={'100%'} sx={{display:'grid',gridTemplateColumns:'repeat(3,1fr)', gap:'24px'}}>
-				<Box sx={{gridColumn:'span 1', border:'1px solid white'}}>Left</Box>
-				<Box sx={{gridColumn:'span 2', border:'1px solid white'}}>Right</Box>
+			<Box width={'100%'} sx={{
+				display:'grid',
+				gridTemplateColumns:'repeat(3,1fr)', 
+				gap:'24px',
+			}}>
+				{/* left side */}
+				<Box sx={{
+					gridColumn:'span 1', 
+					borderTop:'2px solid #077368',
+					p:2,
+					background: 'linear-gradient(180deg, #052531 57.87%, rgba(5, 37, 49, 0.936668) 63.14%, rgba(5, 37, 49, 0.1) 132.68%)',
+				}}>
+					<TotalBalance/>
+					<Typography sx={{color:'white',mt:4, mb:3}}>
+						Addresses
+					</Typography>
+					<AddressBalance/>
+				</Box>
+
+				{/* right side */}
+				<Box sx={{
+					gridColumn:'span 2', 
+					borderTop:'2px solid #F25041',
+					p:2,
+					background: 'linear-gradient(180deg, #052531 57.87%, rgba(5, 37, 49, 0.936668) 63.14%, rgba(5, 37, 49, 0.1) 132.68%)',
+				}}>
+					<TotalBalance/>
+					<Typography sx={{color:'white',mt:4, mb:3}}>
+						Destination Address
+					</Typography>
+					<PasteTextInput sx={{width:'50%'}}/>
+				</Box>
 			</Box>
-			<AddressBalance/>
 		</BasePage>
 	)
 }
