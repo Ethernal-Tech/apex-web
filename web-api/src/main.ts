7eb7f5b0d40e8029a7ceccd939731d531637a61d
Zoltan Babinski <babinskizoltan@gmail.com>
2024-07-29 14:17:11 +0200

    setup cors

--- web-api/src/main.ts
+++ web-api/src/main.ts
@@ -7,7 +7,18 @@ const port = process.env.PORT || 3500;
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.enableCors();
+
+	if (
+		process.env.NODE_ENV === 'production' &&
+		process.env.CORS_ALLOW_LIST !== undefined
+	) {
+		const corsAllowList = process.env.CORS_ALLOW_LIST;
+		const corsOptions = { origin: corsAllowList.split(',') };
+		app.enableCors(corsOptions);
+	} else {
+		app.enableCors(); // Use default CORS settings
+	}
+
 	const config = new DocumentBuilder()
 		.setTitle('Apex Web API')
 		.setDescription('Apex Web  API description')
