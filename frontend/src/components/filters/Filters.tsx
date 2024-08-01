a21092a608c741a4e5274a9af995b4f39d70449e
Nikola <nlukic97@gmail.com>
2024-08-01 13:35:39 +0200

    basic desig for filter made

--- frontend/src/components/filters/Filters.tsx
+++ frontend/src/components/filters/Filters.tsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
 import FilterList from '@mui/icons-material/FilterList';
-import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from '@mui/material';
+import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField, Typography, IconButton } from '@mui/material';
 import AppliedFiltersChips from './AppliedFiltersChips';
 import { BridgeTransactionFilterDto, ChainEnum, TransactionStatusEnum } from '../../swagger/apexBridgeApiService';
 import { capitalizeWord } from '../../utils/generalUtils';
@@ -15,10 +15,13 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    bgcolor: 'background.paper',
-    borderRadius: 2,
+    bgcolor: '#052531',
     boxShadow: 24,
     p: 4,
+    color: 'white',
+    width: '400px',
+    border: '1px solid #435F69',
+    borderRadius: '4px',
 };
 
 type Props = {
@@ -27,7 +30,7 @@ type Props = {
 }
 
 export default function Filters({ filters, onFilterChange }: Props) {
-	const [values, setValues] = useState(new BridgeTransactionFilterDto(filters));
+    const [values, setValues] = useState(new BridgeTransactionFilterDto(filters));
     const [open, setOpen] = useState(false);
 
     const chain = useSelector((state: RootState) => state.chain.chain)
@@ -35,73 +38,73 @@ export default function Filters({ filters, onFilterChange }: Props) {
 
     const destinationChains = useMemo(() => Object.values(ChainEnum).filter(x => x !== chain), [chain])
 
-	const removeFilterCallback = useCallback(
-		(propName: string) => {
-			if (filters.hasOwnProperty(propName)) {
-				onFilterChange(new BridgeTransactionFilterDto({
-					...filters,
-					[propName]: undefined
-				}))
-			}
-		},
-		[filters, onFilterChange]
-	)
+    const removeFilterCallback = useCallback(
+        (propName: string) => {
+            if (filters.hasOwnProperty(propName)) {
+                onFilterChange(new BridgeTransactionFilterDto({
+                    ...filters,
+                    [propName]: undefined
+                }))
+            }
+        },
+        [filters, onFilterChange]
+    )
 
-	useEffect(
-		() => {
-			setValues(new BridgeTransactionFilterDto(filters))
-		},
-		[filters]
-	)
+    useEffect(
+        () => {
+            setValues(new BridgeTransactionFilterDto(filters))
+        },
+        [filters]
+    )
 
     const applyFiltersCallback = useCallback(
-		() => {
-			onFilterChange(new BridgeTransactionFilterDto(values))
-			setOpen(false);
-		},
-		[onFilterChange, values]
-	)
+        () => {
+            onFilterChange(new BridgeTransactionFilterDto(values))
+            setOpen(false);
+        },
+        [onFilterChange, values]
+    )
 
-	const resetFiltersCallback = useCallback(
-		() => {
-			// keep sort
-			onFilterChange(new BridgeTransactionFilterDto({
+    const resetFiltersCallback = useCallback(
+        () => {
+            // keep sort
+            onFilterChange(new BridgeTransactionFilterDto({
                 originChain: chain,
                 senderAddress: accountInfo?.account || '',
-				order: filters.order,
-				orderBy: filters.orderBy,
-			}))
-		},
-		[onFilterChange, chain, accountInfo?.account, filters.order, filters.orderBy]
-	)
+                order: filters.order,
+                orderBy: filters.orderBy,
+            }))
+        },
+        [onFilterChange, chain, accountInfo?.account, filters.order, filters.orderBy]
+    )
 
-	const changeCallback = useCallback(
-		(e:  React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | SelectChangeEvent<TransactionStatusEnum> | SelectChangeEvent<ChainEnum>) => {
-			setValues((state) => new BridgeTransactionFilterDto({
-				...state,
-				page: 0,
-				 [e.target.name]: e.target.value
-			}))
-		},
-		[]
-	)
+    const changeCallback = useCallback(
+        (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | SelectChangeEvent<TransactionStatusEnum> | SelectChangeEvent<ChainEnum>) => {
+            setValues((state) => new BridgeTransactionFilterDto({
+                ...state,
+                page: 0,
+                [e.target.name]: e.target.value
+            }))
+        },
+        []
+    )
 
     return (
         <div>
-            <Box sx={{display: 'flex', alignItems: 'center', justifyContent:'space-between', gap: 1, mb:1 }}>
-                <AppliedFiltersChips filters={filters} removeFilter={removeFilterCallback} resetFilters={resetFiltersCallback}/>
+            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', gap: 1, mb: 1 }}>
+                <AppliedFiltersChips filters={filters} removeFilter={removeFilterCallback} resetFilters={resetFiltersCallback} />
                 <Button
                     variant="outlined"
                     onClick={() => setOpen(true)}
                     sx={{
-                        color:'white',
-                        borderColor:'white',
-                        minWidth:'unset',
-                        padding:'5px',
-                        borderRadius:'100px',
-                        '&:hover':{
-                            borderColor:'#a6a6a6',
-                            color:'#a6a6a6'
+                        color: 'white',
+                        borderColor: 'white',
+                        minWidth: 'unset',
+                        padding: '5px',
+                        borderRadius: '100px',
+                        '&:hover': {
+                            borderColor: '#a6a6a6',
+                            color: '#a6a6a6'
                         },
                     }}
                 >
@@ -125,50 +128,138 @@ export default function Filters({ filters, onFilterChange }: Props) {
                             mb: 2,
                         }}
                     >
+                        <Typography variant="h6" sx={{ textTransform: 'uppercase' }}>Filter</Typography>
+
+                        <InputLabel sx={{color:'white'}} id="destination-chain-label">Destination Chain</InputLabel>
                         <FormControl sx={{ minWidth: 220 }} variant="outlined" size='small'>
-                            <InputLabel id="destination-chain-label">Destination Chain</InputLabel>
                             <Select
-                                labelId="destination-chain-label"
                                 id="destination-chain"
+                                labelId='destination-chain-label'
                                 name='destinationChain'
-                                label="Destination Chain"
                                 variant="outlined"
                                 value={values.destinationChain}
                                 onChange={changeCallback}
+                                sx={{
+                                    '& .MuiSelect-select': {
+                                        backgroundColor: '#1a2e3b',
+                                        borderColor: '#1a2e3b',
+                                    },
+                                    '& .MuiOutlinedInput-notchedOutline': {
+                                        borderColor: '#1a2e3b',
+                                    },
+                                }}
                             >
-								{destinationChains.map(chain => (
-									<MenuItem key={chain} value={chain}>{capitalizeWord(chain)}</MenuItem>
-								))}
+                                {destinationChains.map(chain => (
+                                    <MenuItem key={chain} value={chain}>{capitalizeWord(chain)}</MenuItem>
+                                ))}
                             </Select>
                         </FormControl>
-                        <Box>
-                            <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+
+                        <InputLabel sx={{color:'white'}} id="receiver-address">Receiver Address</InputLabel>
+                        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1, position:'relative' }}>
+                            <TextField
+                                id="receiver-address"
+                                name="receiverAddress"
+                                variant="outlined"
+                                size="small"
+                                // value={values.receiverAddress} // TODO - implement receiverAddress as a filter
+                                onChange={changeCallback}
+                                sx={{
+                                    width:'100%',
+                                    '& .MuiOutlinedInput-root': {
+                                        backgroundColor: '#1a2e3b',
+                                    },
+                                    '& .MuiOutlinedInput-notchedOutline': {
+                                        borderColor: '#1a2e3b',
+                                    },
+                                }}
+                            />
+                            <Button
+                                variant="text"
+                                sx={{
+                                    color: '#F27B50',
+                                    backgroundColor: 'transparent',
+                                    position:'absolute',
+                                    top:'50%',
+                                    transform: 'translateY(-50%)',
+                                    right:0,
+                                    '&:hover': {
+                                        backgroundColor: 'transparent',
+                                        color:'#ed8a66'
+                                    },
+                                }}
+                            >
+                                PASTE
+                            </Button>
+                        </Box>
+
+                        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                            <Box>
+                                <Typography>Amount From</Typography>
                                 <TextField
                                     id="amount-from"
-                                    name={'amountFrom'}
-                                    label="Amount From"
+                                    name='amountFrom'
                                     type="number"
                                     variant="outlined"
                                     size="small"
                                     value={values.amountFrom}
                                     onChange={changeCallback}
+                                    sx={{
+                                        '& .MuiOutlinedInput-root': {
+                                            backgroundColor: '#1a2e3b',
+                                        },
+                                        '& .MuiOutlinedInput-notchedOutline': {
+                                            borderColor: '#1a2e3b',
+                                        },
+                                    }}
                                 />
+                            </Box>
+                            <Box>
+                            <Typography>Amount To</Typography>
                                 <TextField
                                     id="amount-to"
-                                    name={'amountTo'}
-                                    label="Amount To"
+                                    name='amountTo'
                                     type="number"
                                     variant="outlined"
                                     size="small"
                                     value={values.amountTo}
                                     onChange={changeCallback}
+                                    sx={{
+                                        '& .MuiOutlinedInput-root': {
+                                            backgroundColor: '#1a2e3b',
+                                        },
+                                        '& .MuiOutlinedInput-notchedOutline': {
+                                            borderColor: '#1a2e3b',
+                                        },
+                                    }}
                                 />
                             </Box>
                         </Box>
                     </Box>
                     <Box sx={{ display: 'flex', justifyContent: 'flex-end', gap: 2 }}>
-                        <Button onClick={() => setOpen(false)}>Cancel</Button>
-                        <Button variant="contained" color="primary" onClick={applyFiltersCallback}>
+                        <Button
+                            onClick={() => setOpen(false)}
+                            sx={{
+                                color: 'white',
+                                borderColor: '#f0a500',
+                                '&:hover': {
+                                    backgroundColor: 'transparent',
+                                    borderColor: '#f0a500',
+                                },
+                            }}
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            variant="contained"
+                            sx={{
+                                backgroundColor: '#F27B50',
+                                '&:hover': {
+                                    backgroundColor: '#ed8a66',
+                                },
+                            }}
+                            onClick={applyFiltersCallback}
+                        >
                             Apply
                         </Button>
                     </Box>
