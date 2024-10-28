78da79e5ff8513bbf1bac4595dc4c3347ba4ad78
Igor Crevar <crewce@gmail.com>
2024-10-28 15:44:25 +0100

    utxo cacher change

--- cardano-api/api/utils/used_utxo_cacher.go
+++ cardano-api/api/utils/used_utxo_cacher.go
@@ -1,33 +1,36 @@
 package utils
 
 import (
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/Ethernal-Tech/cardano-infrastructure/wallet"
 )
 
+type txInputWithTime struct {
+	wallet.TxInput
+	Time time.Time
+}
+
 type UsedUtxoCacher struct {
 	timeout time.Duration
-	data    map[string]map[string]time.Time
+	data    map[string]map[string]txInputWithTime
 }
 
 func NewUsedUtxoCacher(timeout time.Duration) *UsedUtxoCacher {
 	return &UsedUtxoCacher{
-		data:    map[string]map[string]time.Time{},
+		data:    map[string]map[string]txInputWithTime{},
 		timeout: timeout,
 	}
 }
 
 func (c *UsedUtxoCacher) Add(addr string, txInputs []wallet.TxInput) {
 	var (
-		submap map[string]time.Time
+		submap map[string]txInputWithTime
 		tm     = time.Now().UTC()
 	)
 
 	if child, exists := c.data[addr]; !exists {
-		submap = map[string]time.Time{}
+		submap = map[string]txInputWithTime{}
 		c.data[addr] = submap
 	} else {
 		submap = child
@@ -35,14 +38,17 @@ func (c *UsedUtxoCacher) Add(addr string, txInputs []wallet.TxInput) {
 
 	// remove old ones
 	for k, v := range submap {
-		if tm.Sub(v) >= c.timeout {
+		if tm.Sub(v.Time) >= c.timeout {
 			delete(submap, k)
 		}
 	}
 
 	// add new ones
 	for _, x := range txInputs {
-		submap[x.String()] = tm
+		submap[x.String()] = txInputWithTime{
+			TxInput: x,
+			Time:    tm,
+		}
 	}
 }
 
@@ -58,16 +64,10 @@ func (c *UsedUtxoCacher) Get(addr string) []wallet.TxInput {
 
 	// retrieve those that are still valid (remove expired)
 	for k, v := range submap {
-		if tm.Sub(v) >= c.timeout {
+		if tm.Sub(v.Time) >= c.timeout {
 			delete(submap, k)
 		} else {
-			parts := strings.Split(k, "#")
-			index, _ := strconv.Atoi(parts[1])
-
-			result = append(result, wallet.TxInput{
-				Hash:  parts[0],
-				Index: uint32(index), //nolint:gosec
-			})
+			result = append(result, v.TxInput)
 		}
 	}
 
