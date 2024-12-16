04d5cd702857e9ea8e3e128eb8463afa002ea05d
Nikola Jovićević <nikola.jovicevic@ethernal.tech>
2024-12-16 11:22:23 +0100

    unique constraint and migration

--- web-api/src/bridgeTransaction/bridgeTransaction.entity.ts
+++ web-api/src/bridgeTransaction/bridgeTransaction.entity.ts
@@ -24,7 +24,7 @@ export class BridgeTransaction {
 	@Column({ enum: TransactionStatusEnum, enumName: 'TransactionStatusEnum' })
 	status: TransactionStatusEnum;
 
-	@Column()
+	@Column({ unique: true })
 	sourceTxHash: string;
 
 	@Column({ nullable: true })
