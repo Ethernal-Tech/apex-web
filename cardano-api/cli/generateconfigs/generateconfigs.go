a645d223b9c11d6103e52fce20b1945410c5d93c
dulicd <dulicd@outlook.com>
2024-12-23 17:20:11 +0100

    skyline generate configs

--- cardano-api/cli/generateconfigs/generateconfigs.go
+++ cardano-api/cli/generateconfigs/generateconfigs.go
@@ -1,47 +1,43 @@
 package cligenerateconfigs
 
 import (
-	"fmt"
-
 	"github.com/Ethernal-Tech/cardano-api/common"
 	"github.com/spf13/cobra"
 )
 
-var paramsData = &generateConfigsParams{}
+const skylineUse = "skyline"
+
+var (
+	paramsData        = &generateConfigsParams{}
+	skylineParamsData = &skylineGenerateConfigsParams{}
+)
 
 func GetGenerateConfigsCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "generate-configs",
 		Short:   "generates default config json files",
 		PreRunE: runPreRun,
-		Run:     runCommand,
+		Run:     common.GetCliRunCommand(paramsData),
+	}
+	cmdSkyline := &cobra.Command{
+		Use:     skylineUse,
+		Short:   "generate default config json files for skyline",
+		PreRunE: runPreRun,
+		Run:     common.GetCliRunCommand(skylineParamsData),
 	}
 
 	paramsData.setFlags(cmd)
+	skylineParamsData.setFlags(cmdSkyline)
+
+	cmd.AddCommand(cmdSkyline)
 
 	return cmd
 }
 
-func runPreRun(_ *cobra.Command, _ []string) error {
-	return paramsData.validateFlags()
-}
-
-func runCommand(cmd *cobra.Command, _ []string) {
-	outputter := common.InitializeOutputter(cmd)
-	defer outputter.WriteOutput()
-
-	defer func() {
-		if r := recover(); r != nil {
-			outputter.SetError(fmt.Errorf("%v", r))
-		}
-	}()
-
-	results, err := paramsData.Execute()
-	if err != nil {
-		outputter.SetError(err)
-
-		return
+func runPreRun(cb *cobra.Command, _ []string) error {
+	if cb.Use == skylineUse {
+		return skylineParamsData.validateFlags()
 	}
 
-	outputter.SetCommandResult(results)
+	return paramsData.validateFlags()
 }
--- cardano-api/common/chain_id.go
+++ cardano-api/common/chain_id.go
@@ -6,25 +6,29 @@ const (
 	ChainTypeCardano = iota
 	ChainTypeEVM
 
-	ChainIDStrPrime  = "prime"
-	ChainIDStrVector = "vector"
-	ChainIDStrNexus  = "nexus"
+	ChainIDStrPrime   = "prime"
+	ChainIDStrVector  = "vector"
+	ChainIDStrNexus   = "nexus"
+	ChainIDStrCardano = "cardano"
 
-	ChainIDIntPrime  = chainIDNum(1)
-	ChainIDIntVector = chainIDNum(2)
-	ChainIDIntNexus  = chainIDNum(3)
+	ChainIDIntPrime   = chainIDNum(1)
+	ChainIDIntVector  = chainIDNum(2)
+	ChainIDIntNexus   = chainIDNum(3)
+	ChainIDIntCardano = chainIDNum(4)
 )
 
 var (
 	strToInt = map[string]chainIDNum{
-		ChainIDStrPrime:  ChainIDIntPrime,
-		ChainIDStrVector: ChainIDIntVector,
-		ChainIDStrNexus:  ChainIDIntNexus,
+		ChainIDStrPrime:   ChainIDIntPrime,
+		ChainIDStrVector:  ChainIDIntVector,
+		ChainIDStrNexus:   ChainIDIntNexus,
+		ChainIDStrCardano: ChainIDIntCardano,
 	}
 	intToStr = map[chainIDNum]string{
-		ChainIDIntPrime:  ChainIDStrPrime,
-		ChainIDIntVector: ChainIDStrVector,
-		ChainIDIntNexus:  ChainIDStrNexus,
+		ChainIDIntPrime:   ChainIDStrPrime,
+		ChainIDIntVector:  ChainIDStrVector,
+		ChainIDIntNexus:   ChainIDStrNexus,
+		ChainIDIntCardano: ChainIDStrCardano,
 	}
 )
 
