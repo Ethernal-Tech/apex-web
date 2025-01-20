12a42eadbde6af6fb3a944b2cee0392ef253cc16
Zoltan Babinski <babinskizoltan@gmail.com>
2025-01-20 13:51:15 +0100

    Updated MeshSDK version

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -10,7 +10,8 @@ module.exports = function override(config) {
     "https": require.resolve("https-browserify"),
     "os": require.resolve("os-browserify"),
     "url": require.resolve("url"),
-    "buffer": require.resolve('buffer/')
+    "buffer": require.resolve('buffer/'),
+    "vm": require.resolve('vm-browserify'),
   })
   config.resolve.fallback = fallback;
   config.plugins = (config.plugins || []).concat([
