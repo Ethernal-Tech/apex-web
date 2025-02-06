64a9cc2798a49e7e11fe445bfac461a1109fc31b
Zoltan Babinski <babinskizoltan@gmail.com>
2025-02-06 17:38:31 +0100

    Merge pull request #35 from Ethernal-Tech/terms-and-privacy
    Terms and privacy

---
+++ frontend/src/pages/TermsOfServicePage/TermsOfServicePage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import BasePage from '../base/BasePage';
+import { Box } from "@mui/material";
+import { termsOfServiceHtml } from "./data/TermsOfServicePage.data";
+
+const TermsOfServicePage: React.FC = () => {
+  return (
+    <BasePage>
+      <Box sx={{color:'white'}} 
+        dangerouslySetInnerHTML={{ __html: termsOfServiceHtml }}
+      />
+    </BasePage>
+  );
+};
+
+export default TermsOfServicePage;
