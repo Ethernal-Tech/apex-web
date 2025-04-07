6d32788ebf1138a6e4c40f5aa5644822aa50fc90
dulicd <63311669+dulicd@users.noreply.github.com>
2025-04-07 13:19:12 +0200

    Add isRefund field to webapi (#48)
    * Added InitMigration and IsRefundMigration; removed old migrations
    
    * Added isRefund field
    
    * Update tx state if status or  isRefund changed

---
+++ web-api/src/database/migrations/1743672426954-IsRefund.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class IsRefund1743672426954 implements MigrationInterface {
+    name = 'IsRefund1743672426954'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "bridgeTransactions" ADD "isRefund" boolean NOT NULL DEFAULT false`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "bridgeTransactions" DROP COLUMN "isRefund"`);
+    }
+
+}
