ca3206b601d35c6e68f2171448337b07b138d0eb
Nikola Jovićević <nikola.jovicevic@ethernal.tech>
2025-12-23 11:52:28 +0100

    web api api-key guard

---
+++ web-api/src/common/guards/api-key.guard.ts
@@ -0,0 +1,30 @@
+import {
+	Injectable,
+	CanActivate,
+	ExecutionContext,
+	UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class ApiKeyGuard implements CanActivate {
+	constructor(private readonly configService: ConfigService) {}
+
+	canActivate(context: ExecutionContext): boolean {
+		const request = context.switchToHttp().getRequest();
+
+		const apiKey = request.headers['x-api-key'];
+
+		if (!apiKey) {
+			throw new UnauthorizedException('API key is missing');
+		}
+
+		const validApiKey = this.configService.get<string>('WEB_API_API_KEY');
+
+		if (apiKey !== validApiKey) {
+			throw new UnauthorizedException('Invalid API key');
+		}
+
+		return true;
+	}
+}
