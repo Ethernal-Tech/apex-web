74a559e3240d0e7f2d79fbd5c0d293c22e758399
nlukic97 <nlukic97@gmail.com>
2025-12-26 17:51:04 +0100

    Remove unused theme variables and imports. Final styling on filter modal

--- frontend/src/containers/theme.tsx
+++ frontend/src/containers/theme.tsx
@@ -1,5 +1,1 @@
-import { hexToRgb } from '@mui/material/styles';
-
-export const menuDark = '#051D26';
 export const primaryAccentColor = '#F27B50'; // color of primary button, secondary button border etc
-export const menuOverlay = `rgba(${hexToRgb(menuDark)},0.6)`;
