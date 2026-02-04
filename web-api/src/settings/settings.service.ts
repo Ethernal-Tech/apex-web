921e1086f7f5e8fc1cdc58d42326d68fa427be35
Nikola Jovićević <nikola.jovicevic@ethernal.tech>
2026-02-04 12:12:21 +0100

    remove polygon token

--- web-api/src/settings/settings.service.ts
+++ web-api/src/settings/settings.service.ts
@@ -135,17 +135,6 @@ export class SettingsService {
 			);
 		}
 
-		// TODO: polygon disabled for now
-		// const polTokenID = getCurrencyIDFromDirectionConfig(
-		// 	directionConfig,
-		// 	ChainEnum.Polygon,
-		// );
-		// if (!polTokenID) {
-		// 	throw new Error(
-		// 		`failed to find currencyID for chain: ${ChainEnum.Polygon}`,
-		// 	);
-		// }
-
 		const apexEcosystemToken = ecosystemTokens.find(
 			(x: BridgingSettingsEcosystemTokenDto) => x.id === apexID,
 		);
@@ -171,19 +160,6 @@ export class SettingsService {
 				},
 			};
 		}
-		// TODO: polygon disabled for now
-		// if (!directionConfig[ChainEnum.Polygon]) {
-		// 	directionConfig[ChainEnum.Polygon] = {
-		// 		destChain: {},
-		// 		tokens: {
-		// 			[polTokenID]: {
-		// 				chainSpecific: Lovelace,
-		// 				lockUnlock: true,
-		// 				isWrappedCurrency: false,
-		// 			},
-		// 		},
-		// 	};
-		// }
 
 		for (const [srcChain, dstChains] of Object.entries(
 			reactorSettings.bridgingSettings.allowedDirections,
