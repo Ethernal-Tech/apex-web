caf325d1ddb00cc57306a2e76ba913699646b81d
milos-ethernal <milos.maksimovic@ethernal.tech>
2026-08-13 13:29:53 +0200

    fix critical npm and run format

--- web-api/src/database/migrations/1753373273761-IsRefund.ts
+++ web-api/src/database/migrations/1753373273761-IsRefund.ts
@@ -1,15 +1,18 @@
 /* eslint-disable prettier/prettier */
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class IsRefund1753373273761 implements MigrationInterface {
-    name = 'IsRefund1753373273761'
+	name = 'IsRefund1753373273761';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "bridgeTransactions" ADD "isRefund" boolean NOT NULL DEFAULT false`);
-    }
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`ALTER TABLE "bridgeTransactions" ADD "isRefund" boolean NOT NULL DEFAULT false`,
+		);
+	}
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "bridgeTransactions" DROP COLUMN "isRefund"`);
-    }
-
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`ALTER TABLE "bridgeTransactions" DROP COLUMN "isRefund"`,
+		);
+	}
 }
